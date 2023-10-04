8f05ec97c15b065bb939b6f8aed71d3fc12b6659
Xenos F <git@xenosfio.com>
2023-10-04 14:53:51 +0800

    Hook RemarkCommand into main app

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
 import seedu.address.model.Model;
 
 /**
@@ -9,8 +11,18 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_REMARK + "[REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_REMARK + "Likes to swim.";
+
+   public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Sorry! The remark command has not been implemented yet.";
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult("Hello from remark");
+        return new CommandResult(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
